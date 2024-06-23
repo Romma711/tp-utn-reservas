485714f8daa3f7d8a28ab9207eb38165bef6fba5
Zekencio <eze_reding@outlook.com>
2024-06-23 16:50:37 -0300

    Agregue la struct de empleados, correción de sintaxis y mas

--- main.c
+++ main.c
@@ -2,9 +2,10 @@
 #include <stdlib.h>
 #include <string.h>
 #include "pila.h"
-#define CLIENTES = "clientes.bin"
-#define HABITACIONES = "habitaciones.bin"
-#define RESERVAS = "reservas.bin"
+#define CLIENTES "clientes.bin"
+#define HABITACIONES "habitaciones.bin"
+#define RESERVAS "reservas.bin"
+#define EMPLEADOS "empleados.bin"
 
 const int fil = 3;
 const int col = 3;
@@ -30,13 +31,23 @@ typedef struct{
     int checkIn[2]; ///[0] es para el dia y [1] es para el mes de entrada
     int checkOut[2]; ///[0] es para el dia y [1] es para el mes de salida
 }Reservas;
+
+typedef struct{
+    char nombre[20];
+    char apellido[20];
+    char puesto[30]; ///Recepcionista, conserje, etc.
+    int edad;
+    int dni;
+    int salario;
+}Empleado; ///Esta struct nos puede ser de utilidad más adelante
+
 //TO DO mostrar archivos y sus respectivos contenions de manera recursiva
 
 //TO DO crear una funciones que modifique los registros implementando un arreglo dinamico(en lo posible cargarlo con una funcion void con doble puntero)
 
 //TO DO crear funciones que eliminen los registros de los archivos con un arreglo estatico
 
-///
+///REGION Cuenta de registros
 
 int cantRegistrosCliente(){
     FILE *archi = fopen(CLIENTES,"rb");
@@ -58,7 +69,17 @@ int cantRegistrosReserva(){
     return resultado;
 }
 
-///
+int cantRegistrosEmpleado(){
+    FILE *archi = fopen(EMPLEADOS,"rb");
+    int resultado;
+    if(archi != NULL){
+        fseek(archi,0,SEEK_END);
+        resultado = ftell(archi)/sizeof(Empleado);
+    }
+    return resultado;
+}
+
+///ENDREGION Cuenta de registros
 
 ///REGION Cargar registros
 
@@ -89,8 +110,30 @@ void cargarHabitacion(Habitaciones *aux){
     scanf("%i",aux->costeNoche);
 }
 
+/// Eze: HELL NAW
 void cargarReserva(Reservas *aux){
 
+}
+
+void cargarEmpleado(Empleado *aux){
+    printf("Ingrese el nombre del empleado:\n");
+    fflush(stdin);
+    scanf("%s",aux->nombre);
+    printf("\nIngrese el apellido del empleado:\n");
+    fflush(stdin);
+    scanf("%s",aux->apellido);
+    printf("Ingrese el puesto del empleado:\n");
+    fflush(stdin);
+    scanf("%s",aux->puesto);
+    printf("\nIngrese la edad del empleado:\n");
+    fflush(stdin);
+    scanf("%i",aux->edad);
+    printf("\nIngrese el DNI del empleado:\n");
+    fflush(stdin);
+    scanf("%i",aux->dni);
+    printf("\nIngrese el salario del empleado:\n");
+    fflush(stdin);
+    scanf("%i",aux->salario);
 }
 
 ///ENDREGION Cargar registros
@@ -98,28 +141,37 @@ void cargarReserva(Reservas *aux){
 ///REGION Crear archivos
 
 void crearArchivoCliente(Cliente aux){
-    FILE *archi = fopen(CLIENTES,"wb")
+    FILE *archi = fopen(CLIENTES,"wb");
     if(archi == NULL){
         fwrite(&aux,sizeof(Cliente),1,archi);
-        printf("Se creo el archivos y se almaceno el registro ingresado");
+        printf("Se creo el archivo y se almaceno el registro ingresado");
     }
     fclose(archi);
 }
 
 void crearArchivoHabitaciones(Habitaciones aux){
-    FILE *archi = fopen(HABITACIONES,"wb")
+    FILE *archi = fopen(HABITACIONES,"wb");
     if(archi == NULL){
         fwrite(&aux,sizeof(Habitaciones),1,archi);
-        printf("Se creo el archivos y se almaceno el registro ingresado");
+        printf("Se creo el archivo y se almaceno el registro ingresado");
     }
     fclose(archi);
 }
 
 void crearArchivoReservas(Reservas aux){
-    FILE *archi = fopen(RESERVAS,"wb")
+    FILE *archi = fopen(RESERVAS,"wb");
     if(archi == NULL){
         fwrite(&aux,sizeof(Reservas),1,archi);
-        printf("Se creo el archivos y se almaceno el registro ingresado");
+        printf("Se creo el archivo y se almaceno el registro ingresado");
+    }
+    fclose(archi);
+}
+
+void crearArchivoEmpleado(Empleado aux){
+    FILE *archi = fopen(EMPLEADOS,"wb");
+    if(archi == NULL){
+        fwrite(&aux,sizeof(Empleado),1,archi);
+        printf("Se creo el archivo y se almaceno el registro ingresado");
     }
     fclose(archi);
 }
@@ -144,7 +196,7 @@ int validarFechas(Reservas aValidar,FILE *archi){
     return flag;
 }
 
-void cargarArchivoClientes (){
+void cargarArchivoClientes(){
     Cliente aux,aux2;
     cargarCliente(&aux);
     FILE *archi = fopen(CLIENTES, "r+b");
@@ -152,8 +204,8 @@ void cargarArchivoClientes (){
         int flag=0;
         fseek(archi,0,SEEK_SET);
         while(!feof(archi)){
-            fread(&aux2,sizeof(Cliente),1,archi)
-            if(strcmpi(aux.nombre,aux2.nombre) && strcmpi(aux.apellido,aux2.apellido) && aux.dni==aux2.dni){
+            fread(&aux2,sizeof(Cliente),1,archi);
+            if(strcmpi(aux.nombre,aux2.nombre) == 0 && strcmpi(aux.apellido,aux2.apellido) == 0 && aux.dni==aux2.dni){
                 flag=1;
             }
         }
@@ -169,7 +221,7 @@ void cargarArchivoClientes (){
     fclose(archi);
 }
 
-void cargarArchivoHabitaciones (){
+void cargarArchivoHabitaciones(){
     Habitaciones aux;
     cargarHabitacion(&aux);
     FILE *archi = fopen(HABITACIONES, "ab");
@@ -182,7 +234,7 @@ void cargarArchivoHabitaciones (){
     fclose(archi);
 }
 
-void cargarArchivoReservas (){
+void cargarArchivoReservas(){
     Reservas aux;
     cargarReservas(&aux);
     FILE *archi = fopen(RESERVAS, "r+b");
@@ -198,24 +250,54 @@ void cargarArchivoReservas (){
     fclose(archi);
 }
 
+void cargarArchivoEmpleado(){
+    Empleado aux,aux2;
+    cargarEmpleado(&aux);
+    FILE *archi = fopen(EMPLEADOS, "r+b");
+    if(archi != NULL){
+        int flag=0;
+        fseek(archi,0,SEEK_SET);
+        while(!feof(archi)){
+            fread(&aux2,sizeof(Empleado),1,archi);
+            if(strcmpi(aux.nombre,aux2.nombre) == 0 && strcmpi(aux.apellido,aux2.apellido) == 0 && aux.dni==aux2.dni){
+                flag=1;
+            }
+        }
+        if(flag==0){
+            fwrite(&aux,sizeof(Empleado),1,archi);
+        }else{
+            printf("\nEste empleado ya esta cargado");
+        }
+    }else{
+        fclose(archi);
+        crearArchivoEmpleado(aux);
+    }
+    fclose(archi);
+}
+
 ///ENDREGION Cargar archivos
 
 ///REGION mostrar contenido del archivo
 
 void mostrarCliente(Cliente aux){
+    printf("\n-------------------------");
     printf("\nNombre: %s\n",aux.nombre);
     printf("\nApellido: %s\n",aux.apellido);
     printf("\nEdad: %i\n",aux.edad);
     printf("\nDNI: %i\n",aux.dni);
+    printf("-------------------------");
 }
 
 void mostrarHabitacion(Habitaciones aux){
+    printf("\n-------------------------");
     printf("\nNumero de Habitacion: %i\n",aux.numero);
     printf("\nCantidad de ambientes: %s\n",aux.numero);
     printf("\nCoste por noche: %i\n",aux.costeNoche);
+    printf("-------------------------");
 }
 
 void mostrarReserva(Reservas aux){
+    printf("\n---------------------------------");
     printf("\nLa habitacion reservada es: \n\n");
     mostrarHabitacion(aux.habitacionReservada);
     printf("\nEsta reservada por: \n\n");
@@ -224,25 +306,47 @@ void mostrarReserva(Reservas aux){
     printf("\nEntran el dia %i del %i\n",aux.checkIn[0], aux.checkIn[1]);
     printf("\nSalen el dia %i del %i\n",aux.checkOut[0], aux.checkOut[1]);
     printf("\nEl coste total de la reserva es de: %i\n",aux.costeTotal);
+    printf("---------------------------------");
+}
+
+void mostrarEmpleado(Empleado aux){
+    printf("\n--------------------------------");
+    printf("\nNombre: %s\n", aux.nombre);
+    printf("\nApellido: %s\n", aux.apellido);
+    printf("\nPuesto: %s\n", aux.puesto);
+    printf("\nEdad: %i\n", aux.edad);
+    printf("\nDNI: %i\n", aux.dni);
+    printf("\nSalario: %i\n", aux.salario);
+    printf("--------------------------------");
 }
 
 void mostrarRecursivoCliente(FILE *archi, int i){
     Cliente aux;
     if(!feof(archi)){
         fread(&aux,sizeof(Cliente),1,archi);
-        printf("\n %i.\n",i+1);
+        printf("\nCliente %i:\n",i+1);
         mostrarCliente(aux);
         mostrarRecursivoCliente(&archi,i+1);
     }
 }
 
 void mostrarRecursivoReserva(FILE *archi, int i){
-    Cliente aux;
+    Reservas aux;
     if(!feof(archi)){
         fread(&aux,sizeof(Reservas),1,archi);
-        printf("\n %i.\n",i+1);
+        printf("\nReserva %i:\n",i+1);
         mostrarReserva(aux);
         mostrarRecursivoReserva(&archi,i+1);
+    }
+}
+
+void mostrarRecursivoEmpleado(FILE *archi, int i){
+    Empleado aux;
+    if(!feof(archi)){
+        fread(&aux,sizeof(Empleado),1,archi);
+        printf("\nEmpleado %i:\n",i+1);
+        mostrarEmpleado(aux);
+        mostrarRecursivoEmpleado(&archi,i+1);
     }
 }
 
@@ -258,30 +362,42 @@ void abrirArchivoCliente(){
     }
     fclose(archi);
 }
+
 void abrirArchivoHabitacion(){
-    FILE *archi = fopen(CLIENTES,"rb");
+    FILE *archi = fopen(HABITACIONES,"rb");
     if(archi != NULL){
         Habitaciones aux[fil][col];
         for(int i=0;i<fil;i++){
             for(int j=0;j<col;j++){
-                fread(aux[i][j],sizeof(Habitaciones),1,archi);
+                fread(&aux[i][j],sizeof(Habitaciones),1,archi);
             }
         }
         for(int i=0;i<fil;i++){
             for(int j=0;j<col;j++){
-                mostrarHabitacion(aux[i][j])
+                mostrarHabitacion(aux[i][j]);
             }
         }
     }
     fclose(archi);
 }
+
 void abrirArchivoReservas(){
-    FILE *archi = fopen(CLIENTES,"rb");
+    FILE *archi = fopen(RESERVAS,"rb");
     if(archi != NULL){
 
     }
     fclose(archi);
 }
+
+void abrirArchivoEmpleado(){
+    int i = 0;
+    FILE *archi = fopen(EMPLEADOS,"rb");
+    if(archi != NULL){
+        mostrarRecursivoEmpleado(&archi,i);
+    }
+    fclose(archi);
+}
+
 ///ENDREGION Mostrar contenido del archivo
 
 ///REGION MENU
